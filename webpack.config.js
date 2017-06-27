a4699366550b2f7f85602f1fd177e0cfcd932815
Jay Ullman <jayullman@gmail.com>
2017-06-27 03:07:29 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,29 @@ module.exports = {
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract('css-loader')
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract('css-loader!sass-loader')
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[hash].[ext]',
+            limit: 5000,
+            mimetype: 'application/font-woff'
+          }
+        }
+      }, {
+        test: /\.(ttf|eot|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[hash].[ext]'
+          }
+        }
       }
     ]
   },
